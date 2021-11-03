fcda422699abeb962aa0da46d3318abc6dc103fc
AdamHTang <aht3138@utulsa.edu>
2021-11-03 16:30:53 -0500

    FSM addition

--- Unity-AI/Assets/Scripts/FSMStateType.cs
+++ Unity-AI/Assets/Scripts/FSMStateType.cs
@@ -1,6 +1,15 @@
+/* Author: Adam Tang
+ * Date Created: 11-3-2021
+ * Date Modified: 11-3-2021
+ * Description: States of the Chick
+ * 
+ */
+
 public enum FSMStateType
 {
     None,
     Patrol,
+    Chase,
+    Attack
 
 }
---
+++ Unity-AI/Assets/Scripts/iFSMState.cs
@@ -0,0 +1,10 @@
+/* Author: Adam Tang
+ * Date Created: 11-3-2021
+ * Date Modified: 11-3-2021
+ * Description: Interface for Chick States
+ * 
+ */
+
+public interface IFSMState{
+    FSMStateType StateName { get; }
+}
